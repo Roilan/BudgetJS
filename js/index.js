e31bb39ca4e4491886581dcbf2343f3abd3338fc
Roilan <roilan14@gmail.com>
2015-04-07 15:15:20 -0400

    added chart.js

--- js/index.js
+++ js/index.js
@@ -1,20 +1,48 @@
 (function() {
+	// input
 	var income = document.getElementById('income');
 	var expense = document.getElementById('expense');
 
+	// butttons
 	var submit = document.getElementById('submitBtn');
 	var clear = document.getElementById('clearBtn');
 	var log = document.getElementById('logBtn');
 	
+	// balance
 	var totalIncome = document.getElementById('totalIncome');
 	var totalExpense = document.getElementById('totalExpense');
 	var netIncome = document.getElementById('netIncome');
 
-	// default values
+	// chart
+	var ctx = document.getElementById('myChart').getContext('2d');
+
+	// chart data
+	var chartData = {
+		labels: ['Expense'],
+
+		datasets: [
+			{
+        fillColor: "rgba(151,187,205,0.5)",
+        strokeColor: "rgba(151,187,205,0.8)",
+        highlightFill: "rgba(151,187,205,0.75)",
+        highlightStroke: "rgba(151,187,205,1)",
+        data: [28]
+			}
+		]
+	};
+	var barChart = new Chart(ctx).Bar(chartData);
+
+
+	
+
+	// default balance values
 	totalIncome.innerHTML = 0;
 	totalExpense.innerHTML = 0;
 	netIncome.innerHTML = 0;
 
+
+	
+	// calculate balance functions
 	function calcIncome() {
 		// get income values
 		var incomeVal = income.value;
@@ -26,7 +54,7 @@
 
 		return incomeVal;
 	}
-
+	
 	function calcExpense() {
 		// get expense values
 		var expenseVal = expense.value;
@@ -46,6 +74,8 @@
 
 		//return netIncomeVal;
 	}
+
+
 
 	submit.addEventListener('click', function(e) {
 		// prevent page from refreshing
