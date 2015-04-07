164f42d17ec5f04506b402e6f9a044c9ce2e59d2
Roilan <roilan14@gmail.com>
2015-04-07 07:25:08 -0400

    defaults markup / styles

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 (function() {
+	var income = document.getElementById('income');
+	var expense = document.getElementById('expense');
 
+	var submit = document.getElementById('submitBtn');
+	var clear = document.getElementById('clearBtn');
+	var log = document.getElementById('logBtn');
+	
+	var totalIncome = document.getElementById('totalIncome');
+	var totalExpense = document.getElementById('totalExpense');
+	var netIncome = document.getElementById('netIncome');
 
-	console.log('hello world');
-
-
-
+	
 
 })();
