078c62de34cdbc8e07288c90fc38c0f03c7210e1
Roilan <roilan14@gmail.com>
2015-04-07 08:01:25 -0400

    added functions to calculate income, expense and net

--- js/index.js
+++ js/index.js
@@ -10,6 +10,51 @@
 	var totalExpense = document.getElementById('totalExpense');
 	var netIncome = document.getElementById('netIncome');
 
-	
+	// default values
+	totalIncome.innerHTML = 0;
+	totalExpense.innerHTML = 0;
+	netIncome.innerHTML = 0;
+
+	function calcIncome() {
+		// get income values
+		var incomeVal = income.value;
+
+		// add all income values
+		
+		// set income in HTML
+		totalIncome.innerHTML = incomeVal;
+
+		return incomeVal;
+	}
+
+	function calcExpense() {
+		// get expense values
+		var expenseVal = expense.value;
+
+		// set expense in HTML
+		totalExpense.innerHTML = expenseVal;
+
+		return expenseVal;
+	}
+
+	function calcNetIncome() {
+		// invoke calcIncome and calcExpense
+		var netIncomeVal = calcIncome() - calcExpense();
+
+		// set net income in HTML
+		netIncome.innerHTML = netIncomeVal;
+
+		//return netIncomeVal;
+	}
+
+	submit.addEventListener('click', function(e) {
+		// prevent page from refreshing
+		e.preventDefault();
+		
+		// invoke calc functions
+		calcIncome();
+		calcExpense(); 
+		calcNetIncome();
+	});
 
 })();
